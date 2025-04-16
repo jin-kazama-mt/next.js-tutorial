b6978a147a77e4a88725084b1bd9893878d50818
jin-kazama-mt <jin.kazama@metateam.co.jp>
2025-04-16 15:55:38 +0000

    week2完了

---
+++ src/app/admin/setting/page.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+	title: "管理者設定設定",
+};
+
+/**
+ * ページに表示させる文言を<h1>タグで囲んで返す関数
+ * @returns ページに表示させる文言を<h1>タグで囲んで返す
+ */
+export default function managerSetting() {
+	return (
+		<div>
+			<h1>管理者が設定を行うページ</h1>
+		</div>
+	);
+}
